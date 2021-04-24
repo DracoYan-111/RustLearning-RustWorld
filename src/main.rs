0c89fae48471ccbd8eee059e18239fa1c11c916d
29449 <2944972991@qq.com>
2021-04-24 17:10:40 +0800

    YanLong:rust猜数游戏已完成

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+
 fn main() {
-    println!("Hello, world__________________!");
+    let str = rand::thread_rng().gen_range(1, 101);
+    println!("猜数游戏开始");
+    println!("答案是：{}", str);
+    loop {
+        println!("请输入你的数字");
+        let mut fee = String::new();
+        io::stdin().read_line(&mut fee).expect("接收失败");
+        let fee: u32 = match fee.trim().parse()
+        {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        match fee.cmp(&str) {
+            Ordering::Less => {
+                println!("太小");
+            }
+            Ordering::Equal => {
+                println!("答对了");
+                break;
+            }
+            Ordering::Greater => {
+                println!("太大");
+            }
+        }
+        println!("你的输入是:{}", fee);
+    }
 }
